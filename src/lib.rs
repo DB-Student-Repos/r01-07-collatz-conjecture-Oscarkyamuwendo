e0e7b9e160b7ef044d36d57894e1fdc1acf1c523
Oscar Kyamuwendo <148862985+Oscarkyamuwendo@users.noreply.github.com>
2024-03-13 23:07:26 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
-    )
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+    let mut steps = 0;
+    while n != 1 {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        steps += 1;
+        if steps == u64::MAX {
+            return None; // Overflow protection
+        }
+    }
+    Some(steps)
 }
